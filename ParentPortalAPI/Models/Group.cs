b6fcc9c106e878562724d17261d58ccba30789f6
Emin Bayrak <bayraken@mail.uc.edu>
2017-11-05 21:56:45 -0500

    Created group model and controller. Inserted some groups into group table to retreieve APIs. Nuget update

---
+++ ParentPortalAPI/Models/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParentPortalAPI.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string DistrictId { get; set; }
+        public string UserId { get; set; }
+    }
+}
