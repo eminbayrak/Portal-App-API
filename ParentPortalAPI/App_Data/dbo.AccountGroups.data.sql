2a61a9be4e2dcf467e336c339acac0b970a73578
Matthew McCarragher <mccarrmb@mail.uc.edu>
2018-02-26 17:56:33 -0500

    added test data scripts

---
+++ ParentPortalAPI/App_Data/dbo.AccountGroups.data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[AccountGroups] ([GroupId], [AccountId]) VALUES (1, N'a7dea4ed-c101-42b6-b3c4-071f56c7b448')
+INSERT INTO [dbo].[AccountGroups] ([GroupId], [AccountId]) VALUES (3, N'a7dea4ed-c101-42b6-b3c4-071f56c7b448')
+INSERT INTO [dbo].[AccountGroups] ([GroupId], [AccountId]) VALUES (5, N'a7dea4ed-c101-42b6-b3c4-071f56c7b448')
---
+++ ParentPortalAPI/App_Data/dbo.Groups.data.sql
@@ -0,0 +1,7 @@
+SET IDENTITY_INSERT [dbo].[Groups] ON
+INSERT INTO [dbo].[Groups] ([Id], [Name], [Description], [DistrictId], [UserId]) VALUES (1, N'Girls Soccer', N'Girls Soccer Program', N'1', N'3d84c06f-026b-44a5-ae4c-2778d9e57ef9')
+INSERT INTO [dbo].[Groups] ([Id], [Name], [Description], [DistrictId], [UserId]) VALUES (2, N'Boys Soccer', N'Boys Soccer Program', N'1', N'3d84c06f-026b-44a5-ae4c-2778d9e57ef9')
+INSERT INTO [dbo].[Groups] ([Id], [Name], [Description], [DistrictId], [UserId]) VALUES (3, N'Baseball', N'Boys Baseball Program', N'1', N'3d84c06f-026b-44a5-ae4c-2778d9e57ef9')
+INSERT INTO [dbo].[Groups] ([Id], [Name], [Description], [DistrictId], [UserId]) VALUES (4, N'Football', N'Boys Football Program', N'1', N'3d84c06f-026b-44a5-ae4c-2778d9e57ef9')
+INSERT INTO [dbo].[Groups] ([Id], [Name], [Description], [DistrictId], [UserId]) VALUES (5, N'Track', N'Co-Ed Track and Field Program', N'1', N'3d84c06f-026b-44a5-ae4c-2778d9e57ef9')
+SET IDENTITY_INSERT [dbo].[Groups] OFF
